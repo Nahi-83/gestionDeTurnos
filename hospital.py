74e2d2e220af394976b0b2acb3c35c5d3a9a336a
Nahiara Millahuala <nahi042007@gmail.com>
2025-11-04 09:57:24 -0300

    Cree la clase Hospital

---
+++ hospital.py
@@ -0,0 +1,8 @@
+class Medico:
+    def __init__(self, nombre, direccion, cantConsultorios):
+        self._nombre = nombre 
+        self._direccion = direccion
+        self._cantConsultorios = cantConsultorios
+
+    def mostrarDatos(self):
+        return f"{self._nombre} Dirección:{self._direccion} Cuenta con {self._cantConsultorios} consultorios"
