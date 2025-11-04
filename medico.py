fc9b0f4d9c160499ea8ca0a68d30c37d409a962e
Nahiara Millahuala <nahi042007@gmail.com>
2025-11-04 10:23:28 -0300

    Agregue el método getter en la clase Medico

--- medico.py
+++ medico.py
@@ -7,6 +7,12 @@ class Medico(Persona):
         self._profesion = profesion
         self._matricula = matricula
     
+    def get_profesion(self):
+        return self._profesion
+
+    def get_matricula(self):
+        return self._matricula
+    
     def mostrarDatos(self):
         datosPrincipales = super().mostrarDatos()
         return f"{datosPrincipales} Profesión: {self._profesion} Matrícula: {self._matricula}"
